3b50c3720280c7348c761723f32bc1af8c0ced1b
seav1 <1@seav.eu.org>
2024-10-17 18:16:39 +0800

    Update login.js

--- login.js
+++ login.js
@@ -2,6 +2,7 @@ const { chromium } = require('playwright');
 
 (async () => {
   const browser = await chromium.launch();
+
   const usernames = process.env.USERNAMES.split(',');
   const passwords = process.env.PASSWORDS.split(',');
 
@@ -10,16 +11,16 @@ const { chromium } = require('playwright');
     const page = await context.newPage();
 
     try {
-      await page.goto('https://webhostmost.com/login');
-      await page.fill('input[name="username"]', usernames[i]);
+      await page.goto('https://app.koyeb.com/auth/signin');
+      await page.fill('input[name="email"]', usernames[i]);
       await page.fill('input[name="password"]', passwords[i]);
       await page.click('button[type="submit"]');
-
-      // 使用更具体的等待方法，等待特定元素
-      await page.waitForSelector('selector_for_element_indicating_successful_login', { timeout: 30000 });
       
-      // 或者可以使用 waitForFunction 进行自定义条件等待
-      // await page.waitForFunction(() => document.querySelector('selector_for_element_indicating_successful_login') !== null, { timeout: 30000 });
+      // 增加超时时间或等待特定元素出现
+      await page.waitForNavigation({ waitUntil: 'networkidle', timeout: 600000 });
+
+      // 或者等待某个特定元素出现
+      // await page.waitForSelector('selector_for_element_indicating_successful_login', { timeout: 60000 });
 
       console.log(`用户 ${usernames[i]} 登录成功！`);
     } catch (error) {
