8f8b096dade35108d3335bf784e26f82bc8ba1e4
seav1 <1@seav.eu.org>
2024-10-17 19:36:19 +0800

    Update login.js

--- login.js
+++ login.js
@@ -15,14 +15,12 @@ const { chromium } = require('playwright');
       await page.fill('input[name="username"]', usernames[i]);
       await page.fill('input[name="password"]', passwords[i]);
       await page.click('button[type="submit"]');
-      
-      // 增加超时时间或等待特定元素出现
-      await page.waitForNavigation({ waitUntil: 'networkidle', timeout: 300000 });
 
-      // 或者等待某个特定元素出现
-      // await page.waitForSelector('selector_for_element_indicating_successful_login', { timeout: 60000 });
+      // 检查页面跳转是否成功
+      await page.waitForURL('https://webhostmost.com/clientarea.php', { timeout: 60000 });
 
       console.log(`用户 ${usernames[i]} 登录成功！`);
+      
     } catch (error) {
       console.error(`用户 ${usernames[i]} 登录失败：`, error);
     } finally {
